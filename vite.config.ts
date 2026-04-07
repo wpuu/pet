eb57ae8bcb817b1c739b3897ae2306b0950278a5
wpuu <xgyk@163.com>
2026-04-07 16:20:54 +0800

    First commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,8 @@
-import path from "path";
-import { fileURLToPath } from "url";
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import { viteSingleFile } from "vite-plugin-singlefile";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), viteSingleFile()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
-});
+  base: '/pet/',   // 👉 新手注意：添加这行！前后都有斜杠，名字必须和仓库名一模一样！
+  plugins: [react()],
+})
